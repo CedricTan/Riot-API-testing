3546adfcffe06894e9dd88da60de7f2014a6f856
Cedric Tan <cedrictan2010@gmail.com>
2022-03-23 20:30:11 +0000

    Further testing

--- API_init.py
+++ API_init.py
@@ -1,16 +1,13 @@
 # Ideation document and initial trials
-
-import keys # Needed for API keys safe-storage, placed in .gitignore
-
+# Needed for API keys safe-storage, placed in .gitignore
+import keys
 import cassiopeia as cass
 
 cass.set_riot_api_key(keys.Riot_API_key)
-cass.set_default_region("EUW")
 
-from cass import Summoner
+summoner = cass.get_summoner(name="PewPrew", region="EUW")
+print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
+                                                                          level=summoner.level,
+                                                                          region=summoner.region)) # Test code
 
-pewprew = Summoner(name="PewPrew", region="EUW")
-pewprew.match_history
-
-
-# Test code
+match_history = cass.get_summoner(name="PewPrew", region="EUW").match_history
